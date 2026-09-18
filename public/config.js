9970e6d22067e536cbb37e94fcd5ba5646fa484e
Suryadarmapraja <3tkj2surya@gmail.com>
2026-09-19 00:48:47 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,1 +1,4 @@
-window.SOMBERE_CONFIG={API_URL:'/api'};
+window.SOMBERE_CONFIG = {
+  SUPABASE_URL: "https://yufvdpdaynjqawfierlr.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_JRoR2EE2e0caFUlKfTza4Q_tvWZg181"
+};
